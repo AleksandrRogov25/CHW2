5f0bbff6e1b22e884fab20aa450bc73f308f212a
AleksandrRogov25 <alrogov25@gmail.com>
2023-09-29 00:14:16 +0300

    HomeWork2

---
+++ task1/Program.cs
@@ -0,0 +1,10 @@
+﻿System.Console.Write("Введите трехзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number < 100 || number >= 1000)
+{
+System.Console.WriteLine("Вы ввели не трехзначное число, повторите попытку");
+return;
+}
+System.Console.WriteLine($"Вы ввели: {number}");
+int secondNumber = number / 10 % 10;
+System.Console.WriteLine($"Вторая цифра: {secondNumber}");
